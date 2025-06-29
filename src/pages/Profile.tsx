57f6693b64e04a07e3af6c852b1a5350085af94c
ciara <40663676+nocheerleader@users.noreply.github.com>
2025-06-29 20:57:32 +0100

    Updated Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -73,7 +73,7 @@ export function Profile() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center py-6">
             <div className="flex items-center space-x-3">
-              <div className="bg-blue-700 p-2 rounded-lg">
+              <div className="bg-primary p-2 rounded-lg">
                 <UserIcon className="h-6 w-6 text-white" />
               </div>
               <div>
