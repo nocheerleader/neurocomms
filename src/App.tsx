f068e1615a621575285103ca7c7824c5d31333dc
ciara <40663676+nocheerleader@users.noreply.github.com>
2025-06-30 18:08:19 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -103,6 +103,7 @@ function Router() {
           <Features />
           <HowItWorks />
           <PricingSection />
+          <FAQ /> 
           <CallToAction />
           <Footer />
         </div>
